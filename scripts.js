c555c1dcb3f8fe9cfbde9e855bda7913364b40df
Igor Oliveira <igorsouza.psn@gmail.com>
2022-08-23 17:03:43 +0100

    Timer

---
+++ scripts.js
@@ -0,0 +1,40 @@
+function getTimeSeconds(seconds) {
+  const data = new Date(seconds * 1000);
+  return data.toLocaleTimeString("pt-BR", {
+    hour12: false,
+    timeZone: "GMT",
+  });
+}
+
+const relogio = document.querySelector(".relogio");
+let segundos = 0;
+let timer;
+
+function iniciarRelogio() {
+  timer = setInterval(function () {
+    segundos++;
+    relogio.innerHTML = getTimeSeconds(segundos);
+  }, 1000);
+}
+
+document.addEventListener("click", function (e) {
+  const el = e.target;
+
+  if (el.classList.contains("iniciar")) {
+    clearInterval(timer);
+    iniciarRelogio();
+    relogio.classList.remove("pausado");
+  }
+
+  if (el.classList.contains("pausar")) {
+    clearInterval(timer);
+    relogio.classList.add("pausado");
+  }
+
+  if (el.classList.contains("zerar")) {
+    clearInterval(timer);
+    relogio.innerHTML = "00:00:00";
+    segundos = 0;
+    relogio.classList.remove("pausado");
+  }
+});
